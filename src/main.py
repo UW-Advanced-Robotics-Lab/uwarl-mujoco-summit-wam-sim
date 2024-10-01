1ca3138c1c7bb2b53e0e4505809b64f33ed78a39
arnab <ajoardar@uwaterloo.ca>
2024-10-01 12:16:31 -0400

    - Made the order in which the robots and the wagon were being called in the boolean-array consistent.

--- src/main.py
+++ src/main.py
@@ -64,7 +64,7 @@ def main():
     # Wagon
     wagon_a = rospy.get_param("launch_wagon_a")
     # List of robots to be loaded
-    robot_list = [robot_a, wagon_a, robot_b, robot_c]
+    robot_list = [robot_a, robot_b, robot_c, wagon_a]
     update_rate = int(freq_muj)
     steptime = 1.0/float(update_rate)
     r = rospy.Rate(update_rate)
