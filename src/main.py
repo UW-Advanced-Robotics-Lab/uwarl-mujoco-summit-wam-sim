77c8b4a5b7ecf4f5fed8b4e036a47228b9ac85c9
arnab <ajoardar@uwaterloo.ca>
2025-10-10 10:27:05 -0400

    - Disable camera-recording.     - Not needed.

--- src/main.py
+++ src/main.py
@@ -112,7 +112,7 @@ def main():
     while not rospy.is_shutdown():
         
         # Step engine to progress in time
-        Engine._update(if_camera_preview=True,
+        Engine._update(if_camera_preview=False,
                        if_viewport_preview=True)
         
         # Set simulation time counter +1
